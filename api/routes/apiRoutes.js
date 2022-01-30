f5e491bee87abbaea36866fff4c34cb95c91e90a
Lucas Silva Sousa <lucasssilva021@gmail.com>
2022-01-30 03:54:02 -0300

    eu juro que o proximo vai ser um texto bonito eu to com muito sono

--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const subjectController = require("../controllers/subjects.controller");
 let router = require('express').Router();
 
-router.get("/", subjectController.findAll);
+router.get("/subjects", subjectController.findAll);
 
 module.exports = router;
