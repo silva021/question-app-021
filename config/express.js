669729fcc6f9ac57a638be56c6df67956b0ab34e
Lucas Silva Sousa <lucasssilva021@gmail.com>
2022-01-30 03:51:33 -0300

    testeee

--- config/express.js
+++ config/express.js
@@ -1,9 +1,10 @@
 const express = require('express');
+require('dotenv').config()
 const app = express();
 const config = require('./default.json')
 const routes = require('../api/routes/apiRoutes')
 module.exports = () => {
-    app.set('port', process.env.PORT || config.server.port);
+    app.set('port', process.env.PORT || 8080);
     app.use(express.json()); 
     app.use(express.urlencoded({ extended: true })); 
     app.use(routes)
